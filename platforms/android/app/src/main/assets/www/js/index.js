fc5520c6536bfcf878ff771aa3b1a328d4929c9b
Kertu <kertu.mikk1@gmail.com>
2019-04-11 22:27:30 +0300

    working

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,22 +1,16 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
+let clockContainer 
+let dateContainer
+let changeColorButton 
+let changeBckgrndColorButton 
+let countdown
+let seconds
+let minutes
+let minutesleft
+let secondsleft
+let intervalCntdwn
+
+
+let app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
@@ -32,15 +26,129 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+       
+        init();
     }
 };
 
 app.initialize();
+
+
+
+function init () {
+    
+    clockContainer = document.querySelector('#clock')
+    StyleChange = document.querySelector('#StyleChange') 
+    
+    console.log(clockContainer)
+    dateContainer = document.querySelector('#date')
+    console.log(dateContainer)
+    currentDate()
+    changeColorButton = document.querySelector('#change-color')
+    changeBckgrndColorButton = document.querySelector('#change-bckgrnd-color')
+    startClock()
+  
+    changeColorButton.addEventListener('click', changeColor)
+    changeBckgrndColorButton.addEventListener('click', changeBckgrndColor)
+   
+    
+    document.querySelector('#startCountdown').addEventListener('click',countdownStart)
+    
+  }
+
+
+function countdownStart(){
+    console.log("CLICKED")
+    clearInterval(intervalCntdwn);
+    minutes = parseInt(document.querySelector("#min").value);
+    seconds = parseInt(document.querySelector("#sec").value);
+    if(seconds < 60){
+        let secondsRly = seconds % 60;
+        let minutesAdd = (seconds - secondsRly) / 60;
+        minutes += minutesAdd;
+        seconds = secondsRly;
+
+    }
+    minutesleft = minutes;
+    secondsleft = seconds;
+    /* 
+    console.log(minutes);
+    console.log(seconds); */
+    countdown = document.getElementById("countdown");
+     
+    
+    
+    updateTimer(minutes,seconds)
+    intervalCntdwn = window.setInterval(function () {
+        updateTimer(minutes,seconds)
+    }, 1000)
+}
+function updateTimer(){
+   
+
+   
+    if(secondsleft == 0){
+        secondsleft = 60;
+        minutesleft -=  1;
+    }else{
+        minutesleft = minutesleft ;
+    } 
+    secondsleft -= 1;
+    if(minutesleft < 0){
+        
+        countdown.innerHTML = "Finished!"
+    
+    }else{
+        countdown.innerHTML = minutesleft + ":" + secondsleft;
+    }     
+  
+}
+
+
+    
+function changeColor () {
+    console.log('muudan värvi')
+    let x = document.getElementById("myColor").value;
+  
+    clockContainer.style.color = x;
+    dateContainer.style.color = x;
+    document.querySelector("#timeleft").style.color = x;
+    document.querySelector("#countdown").style.color = x;
+}
+function changeBckgrndColor () {
+    console.log('muudan tausta värvi')
+    let y = document.getElementById("myBckgrndColor").value;
+  
+    document.body.style.background  = y;
+   
+}
+  
+  
+  
+function startClock () {
+    updateClock()
+    
+    window.setInterval(function () {
+        updateClock()
+    }, 1000)
+}
+  
+function updateClock () {
+    let date = new Date()
+    let currentHours = date.getHours()
+    let currentMinutes = date.getMinutes()
+  
+    currentHours = (currentHours < 10 ? "0" : "") + currentHours
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes
+  
+  
+    clockContainer.innerHTML = currentHours+":"+currentMinutes
+}
+function currentDate(){
+    let date = new Date()
+  
+    let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+   
+  
+    dateContainer.innerHTML =date.getDate()+". "+month[date.getMonth()]+" "+date.getFullYear()
+}
